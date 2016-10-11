e0732de299bf7060368433b0ce9cb956df98b1a7
kirilldubinin <dubininkg@gmail.com>
2016-10-11 22:56:20 +0300

    farm

--- src/app/farm/farm.controller.js
+++ src/app/farm/farm.controller.js
@@ -6,13 +6,54 @@
     .controller('FarmController', FarmController);
 
   /** @ngInject */
-  function FarmController() {
+  function FarmController($mdDialog) {
     var vm = this;
     vm.buildings = [
       {
-        name: '#1',
-        type: 'dairy'
+        name: 'Двор#1',
+        cowsCount: 410,
+        groups: [
+          {
+            number: '02',
+            name: 'Первая группа',
+            color: 'blue',
+            cowsCount: 60
+          },
+          {
+            number: '03',
+            name: 'Первая группа',
+            color: 'blue',
+            cowsCount: 54
+          },
+          {
+            number: '04',
+            name: 'Мастит',
+            color: 'red',
+            cowsCount: 12
+          }, {
+            number: '12',
+            name: 'Мастит',
+            color: 'red',
+            cowsCount: 32
+          }
+        ]
       }
     ];
+
+    vm.showGroupDialog = function (group) {
+      $mdDialog.show({
+        controller: 'GroupController',
+        templateUrl: './app/group/group.html',
+        parent: angular.element(document.body),
+        //targetEvent: ev,
+        clickOutsideToClose:true
+        //fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      })
+      .then(function(answer) {
+        
+      }, function() {
+        
+      });
+    }
   }
 })();
