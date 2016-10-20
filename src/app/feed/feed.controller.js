1d5dc2f8e42efc23d4bb4df65fd503ebfd710538
kirilldubinin <dubininkg@gmail.com>
2016-10-20 09:00:55 +0300

    farm

--- src/app/feed/feed.controller.js
+++ src/app/feed/feed.controller.js
@@ -10,29 +10,54 @@
     var vm = this;
     vm.feedItems = [
       {
-        name: 'Сенаж многолетка',
+        name: 'Сенаж',
         source: 'Поле: 123.11 78 ГА',
         composition: 'Люцерна',
         year: '2016',
         weight: 1300,
-        storage: 'Курган #4',
+        opened: true,
+        storage: 'Курган #04',
         done: false
-      },
-      {
+      }, {
         name: 'Силос',
         source: 'Поле: 113.09 120 ГА',
         composition: 'Кукуруза',
         year: '2016',
         weight: 2200,
-        storage: 'Курган #1',
+        storage: 'Траншея #07',
         done: false
-      },{
+      }, {
+        name: 'Сено',
+        source: 'Поле: 23.39 60 ГА',
+        composition: 'Козлятник',
+        year: '2016',
+        weight: 2000,
+        opened: true,
+        storage: 'Сеносклад',
+        done: false
+      }, {
+        name: 'Сенаж',
+        source: 'Поле: 21.40 89 ГА',
+        composition: 'Козлятник',
+        year: '2016',
+        weight: 600,
+        storage: 'Курган #02',
+        done: false
+      }, {
         name: 'Силос',
         source: 'Поле: 113.09 100 ГА',
         composition: 'Кукуруза',
         year: '2014',
         weight: 2200,
-        storage: 'Курган #1',
+        storage: 'Курган #01',
+        done: true
+      }, {
+        name: 'Сенаж',
+        source: 'Поле: 123.11 78 ГА',
+        composition: 'Козлятник',
+        year: '2015',
+        weight: 2000,
+        storage: 'Курган #04',
         done: true
       }
     ];
