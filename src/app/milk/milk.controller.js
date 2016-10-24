fc4a6a4dd6cb6b2b1440d1054eae247ced632c83
kirilldubinin <dubininkg@gmail.com>
2016-10-24 23:28:23 +0300

    farm

--- src/app/milk/milk.controller.js
+++ src/app/milk/milk.controller.js
@@ -9,166 +9,145 @@
   function MilkController() {
     var vm = this;
 
-    var data1 = [];
-
-    for(var i = 0; i < 30; i++) {
-      data1.push(Math.round((8 - Math.random()) * 100) / 100);
-    }
-    for(var i = 0; i < 30; i++) {
-      data1.push(Math.round((6 + Math.random()) * 100) / 100);
-    }
-    for(var i = 0; i < 30; i++) {
-      data1.push(Math.round((6 - Math.random()) * 100) / 100);
-    }   
-
     var data2 = [];
 
     for(var i = 0; i < 30; i++) {
-      data2.push(Math.round((1 + Math.random()) * 100) / 100);
+        data2.push(Math.round((8.5 - Math.random()) * 100) / 100);
     }
     for(var i = 0; i < 30; i++) {
-      data2.push(Math.round((1 - Math.random()) * 100) / 100);
+        data2.push(Math.round((8.5 + Math.random()) * 100) / 100);
     }
     for(var i = 0; i < 30; i++) {
-      data2.push(Math.round((1 + Math.random()) * 100) / 100);
-    }     
+        data2.push(Math.round((7.4 - Math.random()) * 100) / 100);
+    }
+
+    var data3 = [];
+
+    for(var i = 0; i < 30; i++) {
+        data3.push(Math.round((1.5 - Math.random()) * 100) / 100);
+    }
+    for(var i = 0; i < 30; i++) {
+        data3.push(Math.round((1.3 + Math.random()) * 100) / 100);
+    }
+    for(var i = 0; i < 30; i++) {
+        data3.push(Math.round((1.0 - Math.random()) * 100) / 100);
+    }
+
+
+
+    var ration1 = [];
+    var ration2 = [];
+    var ration3 = [];
+    for(var i = 0; i < 31; i++) {
+        ration1.push(10);
+        ration2.push(null);
+        ration3.push(null);
+    }
+    ration2.shift();
+    ration3.shift();
+    for(var i = 0; i < 31; i++) {
+        ration2.push(10);
+        ration3.push(null);
+    }
+    ration3.shift();
+    for(var i = 0; i < 31; i++) {
+        ration3.push(10);
+    }
 
     $('#container').highcharts({
         chart: {
-            
+            /*type: 'area',
+            spacingBottom: 30*/
         },
         title: {
-            text: 'Валовка по дням в зависимости от рациона'
+            text: 'Суточная валовка молока в зависимости от рациона'
         },
-        subtitle: {
-            text: '01 Сентября 2016 - 01 Декабря 2016'
+        /*subtitle: {
+            text: '* Jane\'s banana consumption is unknown',
+            floating: true,
+            align: 'right',
+            verticalAlign: 'bottom',
+            y: 15
         },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 150,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        },*/
         xAxis: {
             type: 'datetime',
+            //pointInterval: 3600000 * 24, // one day
+            //pointStart: Date.UTC(2016, 09, 1, 0, 0, 0),
             labels: {
                 overflow: 'justify'
             }
         },
         yAxis: {
+            min: 1, 
+            max: 10,
             title: {
                 text: 'Тонн'
             },
-            minorGridLineWidth: 0,
-            gridLineWidth: 0,
-            alternateGridColor: null
+            labels: {
+                formatter: function () {
+                    return this.value;
+                }
+            }
         },
         tooltip: {
-            valueSuffix: ' тонн'
+            formatter: function () {
+                return '<b>' + this.series.name + '</b><br/>' +
+                    this.x + ': ' + this.y;
+            }
         },
         plotOptions: {
             spline: {
-                lineWidth: 3,
-                states: {
-                    hover: {
-                        lineWidth: 4
-                    }
-                },
+                fillOpacity: 0.5,
+                pointInterval: 3600000 * 24, // one day
+                pointStart: Date.UTC(2016, 09, 1, 0, 0, 0),
                 marker: {
                     enabled: false
-                },
-                pointInterval: 3600000 * 24, // one hour
-                pointStart: Date.UTC(2016, 09, 1, 0, 0, 0)
-            }
+                }
+            },
+            area: {
+                fillOpacity: 0.5,
+                pointInterval: 3600000 * 24, // one day
+                pointStart: Date.UTC(2016, 09, 1, 0, 0, 0),
+                marker: {
+                    enabled: false
+                }
+            },
+        },
+        credits: {
+            enabled: false
         },
         series: [{
-            type: 'column',
-            name: 'Рацион#02',
-            data: data1
-
+            type: 'area',
+            name: 'Рацион #11.2016',
+            data: ration1
         }, {
+            type: 'area',
+            name: 'Рацион #12.2016',
+            data: ration2
+        }, {
+            type: 'area',
+            name: 'Рацион #13.2016',
+            data: ration3
+        },{
             type: 'spline',
             name: 'Высший сорт',
-            data: data1
-
-        }, {
-          type: 'spline',
-            name: 'Первый сорт',
             data: data2
-        }], 
-        navigation: {
-            menuItemStyle: {
-                fontSize: '10px'
-            }
-        }
-    });
-
-    $('#container').highcharts({
-        chart: {
-            zoomType: 'xy'
-        },
-        title: {
-            text: 'Average Monthly Temperature and Rainfall in Tokyo'
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com'
-        },
-        xAxis: [{
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            crosshair: true
-        }],
-        yAxis: [{ // Primary yAxis
-            labels: {
-                format: '{value}°C',
-                style: {
-                    color: Highcharts.getOptions().colors[1]
-                }
-            },
-            title: {
-                text: 'Temperature',
-                style: {
-                    color: Highcharts.getOptions().colors[1]
-                }
-            }
-        }, { // Secondary yAxis
-            title: {
-                text: 'Rainfall',
-                style: {
-                    color: Highcharts.getOptions().colors[0]
-                }
-            },
-            labels: {
-                format: '{value} mm',
-                style: {
-                    color: Highcharts.getOptions().colors[0]
-                }
-            },
-            opposite: true
-        }],
-        tooltip: {
-            shared: true
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            x: 120,
-            verticalAlign: 'top',
-            y: 100,
-            floating: true,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-        },
-        series: [{
-            name: 'Rainfall',
-            type: 'column',
-            pointWidth: 120,
-            yAxis: 1,
-            data: [8],
-            tooltip: {
-                valueSuffix: ' mm'
-            }
 
         }, {
-            name: 'Temperature',
             type: 'spline',
-            data: [7.0, 6.9, 7.5, 6.5, 7.2, 7.5, 7.2, 6.5, 7.3, 5.3, 5.9, 6.6],
-            tooltip: {
-                valueSuffix: '°C'
-            }
+            name: 'Первый сорт',
+            data: data3
+
         }]
     });
   }
